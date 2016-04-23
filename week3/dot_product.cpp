4ebc1fbcd9285601142a5c3c8458d6b1f09b21cf
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2016-04-23 15:23:14 -0500

    Added solution to minimum dot product

--- week3/dot_product.cpp
+++ week3/dot_product.cpp
@@ -5,10 +5,28 @@
 using std::vector;
 
 long long min_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+  std::sort(a.begin(), a.end(), std::greater<int>());
+  std::sort(b.begin(), b.end());
+  
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
-    result += a[i] * b[i];
+    result += (long long) a[i] * b[i];
+  }
+  return result;
+}
+
+long long min_dot_product_naive(vector<int> a, vector<int> b) {
+  // write your code here
+  long long result = 0;
+  int indexMaxA =0;
+  int indexMinB =0;
+  for (size_t i = 0; i < a.size(); i++) {
+    indexMaxA = std::max_element(a.begin(), a.end()) - a.begin();
+    indexMinB = std::min_element(b.begin(), b.end()) - b.begin();
+        
+    result += (long long) a[indexMaxA] * b[indexMinB];
+    a[indexMaxA] = -1000000;
+    b[indexMinB] = 1000000;
   }
   return result;
 }
@@ -23,5 +41,6 @@ int main() {
   for (size_t i = 0; i < n; i++) {
     std::cin >> b[i];
   }
-  std::cout << min_dot_product(a, b) << std::endl;
+  
+  std::cout << min_dot_product(a, b) << std::endl;  
 }
